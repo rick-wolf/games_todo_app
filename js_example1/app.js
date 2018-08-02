ff69a42268b18c189448f45b768b1edc8ad73e96
rick-wolf <wolf.rick.c@gmail.com>
2018-08-01 17:14:18 -0700

    jquery is cool

--- js_example1/app.js
+++ js_example1/app.js
@@ -1,14 +1,32 @@
 var main = function() {
 	"use strict";
 	
-	$(".comment-input button").on("click", function(event) {
-		var $new_comment = $("<p>");
+	// insert comments into list
+	var addComment = function() {
+		var new_comment = $("<p>");
 		var comment_text = $(".comment-input input").val();
 
 		if (comment_text !== "") {
-			$new_comment.text(comment_text);
-			$(".comments").append($new_comment);
+			new_comment.text(comment_text);
+			new_comment.hide();
+			$(".comments").append(new_comment);
+			new_comment.slideDown();
 			$(".comment-input input").val("");
+		}
+	};
+
+
+	// event listener for button
+	$(".comment-input button").on("click", function(event) {
+		addComment();
+		
+	});
+
+
+	// event listener for enter key
+	$(".comment-input input").on("keypress", function(event) {
+		if (event.keyCode === 13) {
+			addComment();
 		}
 		
 	});
